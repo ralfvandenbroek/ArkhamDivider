425b213be28dacaa6f324dc611836504f2b7e2e7
Vladimir Yazykov <neizerth@gmail.com>
2026-04-02 22:15:15 +0300

    feat: icons and home page

--- slices/pages/home/ui/HomePage/HomePage.tsx
+++ slices/pages/home/ui/HomePage/HomePage.tsx
@@ -2,6 +2,8 @@ import Stack from "@mui/material/Stack";
 import { SingleColumnLayout } from "@/widgets/layout/SingleColumnLayout";
 import { BandsList } from "../BandsList";
 import { DividerList } from "../DividerList";
+import { IconsDownload } from "../IconsDownload";
+import { InvestigatorTokensPreview } from "../InvestigatorTokensPreview";
 
 export function HomePage() {
 	return (
@@ -9,6 +11,8 @@ export function HomePage() {
 			<Stack gap={8} paddingBlock={8}>
 				<DividerList />
 				<BandsList />
+				<InvestigatorTokensPreview />
+				<IconsDownload />
 			</Stack>
 		</SingleColumnLayout>
 	);
