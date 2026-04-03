e13fe6a5c63ce8d407312545758932497576782e
Vladimir Yazykov <neizerth@gmail.com>
2026-04-03 19:38:33 +0300

    fix: core 2026

--- slices/modules/core/icon/shared/config/position/campaign/core_2026.ts
+++ slices/modules/core/icon/shared/config/position/campaign/core_2026.ts
@@ -7,4 +7,28 @@ export default {
 		top: percent(-2),
 		scale: percent(110),
 	},
+	gangs_of_arkham: {
+		left: percent(-4),
+		top: percent(1),
+		scale: percent(90),
+	},
+	cultists_ch2: {
+		left: percent(1),
+		top: percent(1),
+		scale: percent(90),
+	},
+	whippoorwills_ch2: {
+		top: percent(-1),
+		left: percent(-1),
+	},
+	bad_weather: {
+		top: percent(-3),
+		left: percent(1),
+	},
+	bystanders: {
+		top: percent(1),
+		left: percent(1),
+		scale: percent(90),
+	},
+	hallucinations: {},
 } as IconPositionManifest;
